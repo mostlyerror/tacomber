236d2098dfde177d382d7b5e080e5670daaaf11f
Benjamin Poon <benjamintpoon@gmail.com>
2018-10-10 13:18:03 -0600

    also grab make, model, year, trim from listings

--- tacomber/listing.rb
+++ tacomber/listing.rb
@@ -4,14 +4,18 @@ require 'pry'
 require 'json'
 
 module Tacomber
-  module Listing
 
-    def self.fetch(url)
+  module Listing
+    def self.fetch_and_parse url
+      html = fetch url
+      parse html
+    end
+
+    def self.fetch url
       HTTParty.get url
     end
 
-    def self.fetch_and_parse url
-      html = fetch url
+    def self.parse html
       doc = Nokogiri::HTML html
       title = doc.css('span#titletextonly').text&.strip
       subtitle = doc.css('.postingtitletext small').text&.strip
@@ -22,19 +26,20 @@ module Tacomber
       post_body_node = doc.css('#postingbody')
       post_body_node.search('.print-information').remove
       post_body = post_body_node.text&.strip
+      year, make, model, trim = doc.css('.mapAndAttrs .attrgroup:nth-child(3) span').text.split(" ")
       meta = doc.css('.mapAndAttrs .attrgroup:nth-child(3) span')
         .map(&:text)
         .reduce({}) {|acc, txt|
-        k, v = txt.split(': ')
-        acc[k&.strip] = v&.strip
-        acc
-      }.to_json
-      [url, title, subtitle, price, lat, lng, post_body, meta]
+          k, v = txt.split(': ')
+          acc[k&.strip] = v&.strip
+          acc
+        }
+      [url, title, subtitle, year, make, model, trim, price, lat, lng, post_body, meta]
     end
 
     def self.headers
       %i[url title subtitle price lat lng post_body meta]
     end
-
   end
+
 end
