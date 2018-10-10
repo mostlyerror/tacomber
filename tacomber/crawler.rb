e9df904e1919aab5cd8a9dc72042812f6457416c
Benjamin Poon <benjamintpoon@gmail.com>
2018-10-10 10:29:56 -0600

    slightly better flow control & logging

--- tacomber/crawler.rb
+++ tacomber/crawler.rb
@@ -17,25 +17,26 @@ module Tacomber
 
     def get_links offset = 0
       begin
+        puts search_url(offset) if DEBUG
         feed = Feedjira::Feed.fetch_and_parse search_url(offset)
-        print '.'
       rescue Exception => e
         # TODO: figure out which error to rescue from instead of Exception
-        print 'x'
         if @last_err
           puts 'Aborting, encountered two errors in a row.'
           puts [@last_err, e] && exit(0)
         else
           @last_err = e
+          print 'x'
         end
       end
 
-      if feed.entries.empty?
-        puts "#{offset + 1} pages found" && exit(1)
+      unless feed.entries.empty?
+        print '.'
+        feed.entries.each { |item| @fd.puts item.url }
+        get_links(offset+1)
+      else
+        puts "finished, #{offset+1} pages found"
       end
-
-      feed.entries.each { |item| @fd.puts item.url }
-      get_links offset + 1
     ensure
       @fd.close
     end
@@ -43,7 +44,7 @@ module Tacomber
     # TODO: make me configurable based on search parameters
     # guess we need a SearchParamsBuilder module..
     def search_url offset
-      "https://denver.craigslist.org/search/cta?auto_make_model=toyota+tacoma&bundleDuplicates=1&auto_title_status=1&hasPic=1&format=rss&s=#{offset}"
+      "https://denver.craigslist.org/search/cta?hasPic=1&bundleDuplicates=1&auto_make_model=toyota+tacoma&auto_title_status=1&format=rss&s=#{offset}"
     end
   end
 end
