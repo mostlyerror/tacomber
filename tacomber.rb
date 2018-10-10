76fae4bb6e97b7eb2192d165d7c2deb2265181ff
Benjamin Poon <benjamintpoon@gmail.com>
2018-10-10 11:44:29 -0600

    re-organize module to help manifest path resolution

---
+++ tacomber.rb
@@ -0,0 +1,9 @@
+module Tacomber
+  def self.base_dir
+    @base_dir ||= Dir.pwd
+  end
+end
+
+require_relative './tacomber/crawler'
+require_relative './tacomber/manifest'
+require_relative './tacomber/listing'
--- tacomber/manifest.rb
+++ tacomber/manifest.rb
@@ -1,18 +1,17 @@
 module Tacomber
   module Manifest
     def self.urls
-      find_latest_manifest
-      File.foreach(@manifest_filename) do |line|
+      File.foreach(manifest_filename) do |line|
         yield line.strip
       end
     end
 
-    def self.find_latest_manifest
-      @manifest_filename = Dir.glob('../urls/*').sort.last
+    def self.manifest_filename
+      @filename ||= Dir.glob("#{Tacomber.base_dir}/urls/*").sort.last
     end
 
     def self.size
-      `wc -l < #{@manifest_filename}`.to_i
+      `wc -l < #{manifest_filename}`.to_i
     end
   end
 end
